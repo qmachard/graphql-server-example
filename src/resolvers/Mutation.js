63eed9b0b3119b5eaf0505ef9740b5c58d9273a4
Quentin MACHARD <quentin.machard@gmail.com>
2020-01-26 16:28:36 +0100

    Add 2 methods to signup and login

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const {APP_SECRET} = require('../utils');
+
 const addLink = (root, args, context) => {
   return context.prisma.createLink({
     url: args.url,
@@ -23,8 +28,49 @@ const deleteLink = (parent, args, context) => {
   });
 };
 
+const signup = async (parent, args, context) => {
+  const password = await bcrypt.hash(args.password, 10);
+
+  const user = await context.prisma.createUser({
+    ...args,
+    password,
+  });
+
+  const token = jwt.sign({
+    userId: user.id,
+  }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  }
+};
+
+const login = async (parent, args, context) => {
+  const user = await context.prisma.user({email: args.email});
+  if (!user) {
+    throw new Error('No such user found');
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error('Invalid password');
+  }
+
+  const token = jwt.sign({
+    userId: user.id
+  }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  }
+};
+
 module.exports = {
   addLink,
   updateLink,
   deleteLink,
+  signup,
+  login,
 };
