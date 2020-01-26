50056aac650e4e5487daaf16e8e2899cfcc9cf31
Quentin MACHARD <quentin.machard@gmail.com>
2020-01-26 16:42:01 +0100

    Add query to get all users

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,11 @@ const feed = (root, args, context) => {
   return context.prisma.links();
 };
 
+const allUsers = (root, args, context) => {
+  return context.prisma.users();
+};
+
 module.exports = {
   feed,
+  allUsers,
 };
